bde0ef25f02838dcaa476adc9d9a931e5cf9ceb1
Fukuda1997 <orange.pai4649@gmail.com>
2021-02-15 01:49:23 +0000

    finish 3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cd6b04de1332cdab0960bcdb01519ad0abdc724f1174cd87baffd123a4e2907d59b0fe01d98ca6248851deea5618993c2e56ce4d52a3fa294979046334439b87'
